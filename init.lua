2829802fb786bf977e2283be14f625d40033c47f
sNj <sh1njiteita@gmail.com>
2024-09-14 19:58:39 +0300

    Killed commenets inside init.lua

--- init.lua
+++ init.lua
@@ -4,7 +4,10 @@ require("autocmd")
 
 require("lazy").setup({
 
-	{ "nvim-tree/nvim-tree.lua" },
+	{
+		"nvim-tree/nvim-tree.lua",
+		opts = {},
+	},
 	{
 		"ThePrimeagen/harpoon",
 		branch = "harpoon2",
@@ -94,12 +97,6 @@ require("lazy").setup({
 			vim.api.nvim_create_autocmd("LspAttach", {
 				group = vim.api.nvim_create_augroup("kickstart-lsp-attach", { clear = true }),
 				callback = function(event)
-					-- NOTE: Remember that Lua is a real programming language, and as such it is possible
-					-- to define small helper and utility functions so you don't have to repeat yourself.
-					--
-					--
-					--
-
 					local client = vim.lsp.get_client_by_id(event.data.client_id)
 
 					keymaps_module.init_lspconfig_keymaps(event, client)
@@ -119,15 +116,14 @@ require("lazy").setup({
 							group = highlight_augroup,
 							callback = vim.lsp.buf.clear_references,
 						})
+						vim.api.nvim_create_autocmd("LspDetach", {
+							group = vim.api.nvim_create_augroup("kickstart-lsp-detach", { clear = true }),
+							callback = function(event)
+								vim.lsp.buf.clear_references()
+								vim.api.nvim_clear_autocmds({ group = "kickstart-lsp-highlight", buffer = event.buf })
+							end,
+						})
 					end
-				end,
-			})
-
-			vim.api.nvim_create_autocmd("LspDetach", {
-				group = vim.api.nvim_create_augroup("kickstart-lsp-detach", { clear = true }),
-				callback = function(event)
-					vim.lsp.buf.clear_references()
-					vim.api.nvim_clear_autocmds({ group = "kickstart-lsp-highlight", buffer = event.buf })
 				end,
 			})
 
@@ -147,16 +143,11 @@ require("lazy").setup({
 				cmake = {},
 				pyright = {},
 				lua_ls = {
-					-- cmd = {...},
-					-- filetypes = { ...},
-					-- capabilities = {},
 					settings = {
 						Lua = {
 							completion = {
 								callSnippet = "Replace",
 							},
-							-- You can toggle below to ignore Lua_LS's noisy `missing-fields` warnings
-							-- diagnostics = { disable = { 'missing-fields' } },
 						},
 					},
 				},
@@ -229,12 +220,12 @@ require("lazy").setup({
 				end)(),
 				dependencies = {
 					--    https://github.com/rafamadriz/friendly-snippets
-					-- {
-					--   'rafamadriz/friendly-snippets',
-					--   config = function()
-					--     require('luasnip.loaders.from_vscode').lazy_load()
-					--   end,
-					-- },
+					{
+						"rafamadriz/friendly-snippets",
+						config = function()
+							require("luasnip.loaders.from_vscode").lazy_load()
+						end,
+					},
 				},
 			},
 			"saadparwaiz1/cmp_luasnip",
@@ -261,39 +252,14 @@ require("lazy").setup({
 				--
 				-- No, but seriously. Please read `:help ins-completion`, it is really good!
 				mapping = cmp.mapping.preset.insert({
-					-- Select the [n]ext item
-					["<C-n>"] = cmp.mapping.select_next_item(),
-					-- Select the [p]revious item
-					["<C-p>"] = cmp.mapping.select_prev_item(),
 
-					-- Scroll the documentation window [b]ack / [f]orward
+					["<C-n>"] = cmp.mapping.select_next_item(),
+					["<C-p>"] = cmp.mapping.select_prev_item(),
 					["<C-b>"] = cmp.mapping.scroll_docs(-4),
 					["<C-f>"] = cmp.mapping.scroll_docs(4),
-
-					-- Accept ([y]es) the completion.
-					--  This will auto-import if your LSP supports it.
-					--  This will expand snippets if the LSP sent a snippet.
 					["<C-y>"] = cmp.mapping.confirm({ select = true }),
-
-					-- If you prefer more traditional completion keymaps,
-					-- you can uncomment the following lines
-					--['<CR>'] = cmp.mapping.confirm { select = true },
-					--['<Tab>'] = cmp.mapping.select_next_item(),
-					--['<S-Tab>'] = cmp.mapping.select_prev_item(),
-
-					-- Manually trigger a completion from nvim-cmp.
-					--  Generally you don't need this, because nvim-cmp will display
-					--  completions whenever it has completion options available.
 					["<C-Space>"] = cmp.mapping.complete({}),
 
-					-- Think of <c-l> as moving to the right of your snippet expansion.
-					--  So if you have a snippet that's like:
-					--  function $name($args)
-					--    $body
-					--  end
-					--
-					-- <c-l> will move you to the right of each of the expansion locations.
-					-- <c-h> is similar, except moving you backwards.
 					["<C-l>"] = cmp.mapping(function()
 						if luasnip.expand_or_locally_jumpable() then
 							luasnip.expand_or_jump()
@@ -304,9 +270,6 @@ require("lazy").setup({
 							luasnip.jump(-1)
 						end
 					end, { "i", "s" }),
-
-					-- For more advanced Luasnip keymaps (e.g. selecting choice nodes, expansion) see:
-					--    https://github.com/L3MON4D3/LuaSnip?tab=readme-ov-file#keymaps
 				}),
 				sources = {
 					{ name = "nvim_lsp" },
@@ -340,15 +303,36 @@ require("lazy").setup({
 	{
 		"ellisonleao/gruvbox.nvim",
 		priority = 1000,
-		-- config = true,
-		opt = {
-			-- transparent = true,
-			transparent_mode = true,
-			styles = {
-				sidebars = "transparent",
-				float = "transparent",
-			},
-		},
+		event = "VimEnter",
+		config = function()
+			require("gruvbox").setup({
+				terminal_colors = true, -- add neovim terminal colors
+				undercurl = true,
+				underline = true,
+				bold = true,
+				italic = {
+					strings = true,
+					emphasis = true,
+					comments = true,
+					operators = false,
+					folds = true,
+				},
+				strikethrough = true,
+				invert_selection = false,
+				invert_signs = false,
+				invert_tabline = false,
+				invert_intend_guides = false,
+				inverse = true, -- invert background for search, diffs, statuslines and errors
+				contrast = "", -- can be "hard", "soft" or empty string
+				palette_overrides = {},
+				overrides = {},
+				dim_inactive = false,
+				transparent_mode = true,
+			})
+			vim.cmd("colorscheme gruvbox")
+			-- vim.o.background = "dark"
+			-- vim.o.background = "light"
+		end,
 	},
 
 	-- Highlight todo, notes, etc in comments
@@ -359,7 +343,7 @@ require("lazy").setup({
 		opts = { signs = false },
 	},
 
-	{ -- Collection of various small independent plugins/modules
+	{
 		"echasnovski/mini.nvim",
 		config = function()
 			-- Better Around/Inside textobjects
@@ -473,111 +457,12 @@ require("lazy").setup({
 	},
 })
 
--- nvim-tree
 vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
-
--- optionally enable 24-bit colour
 vim.opt.termguicolors = true
 
 -- empty setup using defaults
-require("nvim-tree").setup()
 
--- OR setup with some options
--- erdhver("aivz-gerr").frghc({
---   -- fbeg = {
---   --   fbegre = "pnfr_frafvgvir",
---   -- },
---   ivrj = {
---     jvqgu = 30,
---   },
---   eraqrere = {
---     tebhc_rzcgl = gehr,
---   },
---   svygref = {
---     qbgsvyrf = gehr,
---   },
--- })
--- api.tree.remove_keymap('<C-t>')
-on_attach = function(bufnr)
-	-- local api = require "nvim-tree.api"
-	--
-	-- local function opts(desc)
-	--   return { desc = "nvim-tree: " .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
-	-- end
-	--
-	-- api.config.mappings.default_on_attach(bufnr)
-	-- api.config.mappings.remove_keymap('<C-t>')
-	-- default mappings
-
-	api.config.mappings.default_on_attach(bufnr)
-
-	vim.keymap.set("n", "<C-T>", "", { buffer = bufnr })
-	vim.keymap.del("n", "<C-T>", { buffer = bufnr })
-	vim.keymap.set("n", "<tab>", "", { buffer = bufnr })
-	vim.keymap.del("n", "<tab>", { buffer = bufnr })
-
-	-- custom mappings
-end
-
--- require'lspconfig'.glsl_analyzer.setup{
---     filetypes = {'vert', 'glsl', 'frag', 'geom'},
--- }
 vim.api.nvim_set_keymap("n", "<C-t>", ":NvimTreeToggle()<CR>", { noremap = true, silent = true })
-
--- vim.o.background = "dark" -- or "light" for light mode
--- vim.cmd([[colorscheme gruvbox]])
-
-require("gruvbox").setup({
-	terminal_colors = true, -- add neovim terminal colors
-	undercurl = true,
-	underline = true,
-	bold = true,
-	italic = {
-		strings = true,
-		emphasis = true,
-		comments = true,
-		operators = false,
-		folds = true,
-	},
-	strikethrough = true,
-	invert_selection = false,
-	invert_signs = false,
-	invert_tabline = false,
-	invert_intend_guides = false,
-	inverse = true, -- invert background for search, diffs, statuslines and errors
-	contrast = "", -- can be "hard", "soft" or empty string
-	palette_overrides = {},
-	overrides = {},
-	dim_inactive = false,
-	transparent_mode = true,
-})
-vim.cmd("colorscheme gruvbox")
 vim.api.nvim_create_user_command("Wqa", "wqa", {})
 vim.api.nvim_create_user_command("Wa", "wa", {})
-
--- require'lspconfig'.glsl_analyzer.setup{}
-
--- local function my_on_attach(bufnr)
---   local api = require "nvim-tree.api"
---
---   local function opts(desc)
---     return { desc = "nvim-tree: " .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
---   end
---
---   -- default mappings
---   api.config.mappings.default_on_attach(bufnr)
---
---   -- custom mappings
---   vim.keymap.set('n', '<C-t>',     api.tree.NvimTreeToggle,                  opts('Open tree'))
--- end
---
--- -- pass to setup along with your other options
--- require("nvim-tree").setup {
---   ---
---   on_attach = my_on_attach,
---   ---
--- }
-
--- The line beneath this is called `modeline`. See `:help modeline`
--- vim: ts=2 sts=2 sw=2 et
