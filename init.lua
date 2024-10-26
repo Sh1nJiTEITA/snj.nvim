eddb3d20e546f7ebd7d069dade8d64b1a00b316b
sNj <Sh1nJiTEITA.TWSOL@gmail.com>
2024-10-27 01:58:00 +0300

    Changed nvim-tree -> neo-tree
    Added css/color adapter

--- init.lua
+++ init.lua
@@ -4,8 +4,109 @@ require("autocmd")
 
 require("lazy").setup({
    {
-      "nvim-tree/nvim-tree.lua",
-      opts = {},
+      "NvChad/nvim-colorizer.lua",
+      -- config = function()
+      -- require("colorizer").setup({
+      opts = {
+         RGB = true, -- #RGB hex codes
+         RRGGBB = true, -- #RRGGBB hex codes
+         names = false, -- "Name" codes like Blue or blue
+         RRGGBBAA = false, -- #RRGGBBAA hex codes
+         AARRGGBB = false, -- 0xAARRGGBB hex codes
+         rgb_fn = false, -- CSS rgb() and rgba() functions
+         hsl_fn = false, -- CSS hsl() and hsla() functions
+         css = true, -- Enable all CSS features: rgb_fn, hsl_fn, names, RGB, RRGGBB
+         css_fn = false, -- Enable all CSS *functions*: rgb_fn, hsl_fn
+         -- Available modes for `mode`: foreground, background,  virtualtext
+         mode = "foreground", -- Set the display mode.
+         -- Available methods are false / true / "normal" / "lsp" / "both"
+         -- True is same as normal
+         tailwind = false, -- Enable tailwind colors
+         -- parsers can contain values used in |user_default_options|
+         sass = { enable = false, parsers = { "css" } }, -- Enable sass colors
+         virtualtext = "■",
+         -- update color values even if buffer is not focused
+         -- example use: cmp_menu, cmp_docs
+         always_update = true,
+      },
+      -- )
+      -- end,
+   },
+
+   -- {
+   --    "brenoprata10/nvim-highlight-colors",
+   --    config = function()
+   --       require("nvim-highlight-colors").setup({})
+   --    end,
+   -- },
+
+   -- {
+   --    "nvim-tree/nvim-tree.lua",
+   --    opts = {},
+   -- },
+   --
+
+   -- {
+   --    "3rd/image.nvim",
+   --    dependencies = {
+   --       "vhyrro/luarocks.nvim",
+   --    },
+   --    opts = {
+   --       backend = "kitty",
+   --       --    integrations = {
+   --       --       markdown = {
+   --       --          enabled = true,
+   --       --          clear_in_insert_mode = false,
+   --       --          download_remote_images = true,
+   --       --          only_render_image_at_cursor = false,
+   --       --          filetypes = { "markdown", "vimwiki" }, -- markdown extensions (ie. quarto) can go here
+   --       --       },
+   --       --       neorg = {
+   --       --          enabled = true,
+   --       --          clear_in_insert_mode = false,
+   --       --          download_remote_images = true,
+   --       --          only_render_image_at_cursor = false,
+   --       --          filetypes = { "norg" },
+   --       --       },
+   --       --       html = {
+   --       --          enabled = false,
+   --       --       },
+   --       --       css = {
+   --       --          enabled = false,
+   --       --       },
+   --       --    },
+   --       max_width = nil,
+   --       max_height = nil,
+   --       max_width_window_percentage = nil,
+   --       max_height_window_percentage = 50,
+   --       window_overlap_clear_enabled = false, -- toggles images when windows are overlapped
+   --       window_overlap_clear_ft_ignore = { "cmp_menu", "cmp_docs", "" },
+   --       editor_only_render_when_focused = false, -- auto show/hide images when the editor gains/looses focus
+   --       tmux_show_only_in_active_window = false, -- auto show/hide images in the correct Tmux window (needs visual-activity off)
+   --       hijack_file_patterns = { "*.png", "*.jpg", "*.jpeg", "*.gif", "*.webp", "*.avif" },
+   --    },
+   -- },
+
+   {
+      "nvim-neo-tree/neo-tree.nvim",
+      branch = "v3.x",
+      dependencies = {
+         "nvim-lua/plenary.nvim",
+         "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+         "MunifTanjim/nui.nvim",
+         "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
+         "s1n7ax/nvim-window-picker",
+      },
+      opts = {
+         window = {
+            position = "left",
+            width = 30,
+            mapping_options = {
+               noremap = true,
+               nowait = true,
+            },
+         },
+      },
    },
    {
       "ThePrimeagen/harpoon",
