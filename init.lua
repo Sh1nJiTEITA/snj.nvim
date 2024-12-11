f1b2eb0cbaa88aa2eaee48302077d776df2bafa3
sNj <Sh1nJiTEITA.TWSOL@gmail.com>
2024-12-11 23:40:15 +0300

    Added cpp-debugging

--- init.lua
+++ init.lua
@@ -4,6 +4,137 @@ require("autocmd")
 
 require("lazy").setup({
    {
+      "Shatur/neovim-cmake",
+      dap_configuration = {
+         type = "codelldb",
+         request = "launch",
+         stopOnEntry = false,
+         runInTerminal = false,
+      },
+   },
+
+   {
+      "rcarriga/nvim-dap-ui",
+      event = "VeryLazy",
+      dependencies = {
+         "mfussenegger/nvim-dap",
+      },
+      config = function()
+         local dap = require("dap")
+         local dapui = require("dapui")
+         dap.listeners.after.event_initialized["dapui_config"] = function()
+            dapui.open()
+         end
+
+         dap.listeners.before.event_terminated["dapui_config"] = function()
+            dapui.close()
+         end
+
+         dap.listeners.before.event_exited["dapui_config"] = function()
+            dapui.close()
+         end
+
+         require("dapui").setup()
+      end,
+   },
+   {
+      "mfussenegger/nvim-dap",
+      dependencies = {
+         "nvim-neotest/nvim-nio",
+      },
+      config = function()
+         local dap = require("dap")
+
+         vim.keymap.set("n", "<F5>", dap.continue, { desc = "Dap: Continue" })
+         vim.keymap.set("n", "<F10>", dap.step_over, { desc = "Dap: Step Over" })
+         vim.keymap.set("n", "<F11>", dap.step_into, { desc = "Dap: Step Into" })
+         vim.keymap.set("n", "<F12>", dap.step_out, { desc = "Dap: Step Out" })
+         vim.keymap.set("n", "<Leader>b", dap.toggle_breakpoint, { desc = "Dap: Toggle Breakpoint" })
+         vim.keymap.set("n", "<Leader>B", function()
+            dap.set_breakpoint(vim.fn.input("Breakpoint condition: "))
+         end, { desc = "Dap: Conditional Breakpoint" })
+         vim.keymap.set("n", "<Leader>lp", function()
+            dap.set_breakpoint(nil, nil, vim.fn.input("Log point message: "))
+         end, { desc = "Dap: Log Point" })
+         vim.keymap.set("n", "<Leader>dr", dap.repl.open, { desc = "Dap: Open REPL" })
+         vim.keymap.set("n", "<Leader>dl", dap.run_last, { desc = "Dap: Run Last" })
+
+         dap.configurations.cpp = {
+            {
+               name = "Launch",
+               type = "codelldb",
+               request = "launch",
+               program = function()
+                  local path = vim.g.dap_cpp_path
+                  if path == nil then
+                     path = vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/", "file")
+                     vim.g.dap_cpp_path = path
+                  end
+                  return path
+               end,
+               cwd = "${workspaceFolder}",
+               stopOnEntry = false,
+               args = {},
+            },
+         }
+
+         -- dap.adapters.cppdbg = {
+         --    id = "cppdbg",
+         --    type = "executable",
+         --    command = "/home/snj/.local/share/nvim/mason/packages/cpptools/extension/debugAdapters/bin/OpenDebugAD7",
+         -- }
+         --
+         -- dap.configurations.cpp = {
+         --    {
+         --       name = "Launch file",
+         --       type = "cppdbg",
+         --       request = "launch",
+         --       program = function()
+         --          local executable = vim.fn.findfile("my_program", vim.fn.getcwd() .. "/build")
+         --          if executable == "" then
+         --             return vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/build/", "file")
+         --          end
+         --          return executable
+         --       end,
+         --       cwd = "${workspaceFolder}",
+         --       stopAtEntry = false,
+         --       setupCommands = {
+         --          {
+         --             description = "Enable pretty-printing for gdb",
+         --             text = "-enable-pretty-printing",
+         --             ignoreFailures = true,
+         --          },
+         --       },
+         --    },
+         -- }
+         --
+         -- -- dap.listeners.before.attach.dapui_config = function()
+         -- --    dapui.open()
+         -- -- end
+         -- -- dap.listeners.before.launch.dapui_config = function()
+         -- --    dapui.open()
+         -- -- end
+         -- -- dap.listeners.before.event_terminated.dapui_config = function()
+         -- --    dapui.close()
+         -- -- end
+         -- -- dap.listeners.before.event_exited.dapui_config = function()
+         -- --    dapui.close()
+         -- -- end
+         --
+         -- dap.listeners.after.event_initialized["dapui_config"] = function()
+         --    dapui.open()
+         -- end
+         --
+         -- dap.listeners.before.event_terminated["dapui_config"] = function()
+         --    dapui.close()
+         -- end
+         --
+         -- dap.listeners.before.event_exited["dapui_config"] = function()
+         --    dapui.close()
+         -- end
+      end,
+   },
+   {
       "kdheepak/lazygit.nvim",
       lazy = false,
       cmd = {
@@ -208,6 +339,21 @@ require("lazy").setup({
       end,
    },
 
+   {
+      "jayp0521/mason-nvim-dap",
+      event = "VeryLazy",
+      dependencies = {
+         "williamboman/mason.nvim",
+         "mfussenegger/nvim-dap",
+      },
+      opts = {
+         handlers = {},
+         -- ensure_installed = {
+         --    "codelldb"
+         -- }
+      },
+   },
+
    { -- LSP Configuration & Plugins
       "neovim/nvim-lspconfig",
       dependencies = {
@@ -262,9 +408,9 @@ require("lazy").setup({
             glsl_analyzer = {
                filetypes = { "vert", "frag", "glsl", "geom" },
             },
-
+            cpptools = {},
             biome = {},
-
+            codelldb = {},
             cmakelang = {},
             cmake = {},
             pyright = {},
@@ -294,6 +440,9 @@ require("lazy").setup({
                   require("lspconfig")[server_name].setup(server)
                end,
             },
+         })
+         require("mason-nvim-dap").setup({
+            ensure_installed = { "cpptools" },
          })
       end,
    },
