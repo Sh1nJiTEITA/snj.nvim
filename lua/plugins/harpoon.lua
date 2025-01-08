a8a5b31a4e3566f58f71e7549e2ca303ad447a39
sNj <Sh1nJiTEITA.TWSOL@gmail.com>
2025-01-09 01:03:03 +0300

    Added new bindings to harpoon2

--- lua/plugins/harpoon.lua
+++ lua/plugins/harpoon.lua
@@ -1,3 +1,5 @@
+IsHarpoonMenuOpen = false
+
 return {
    "ThePrimeagen/harpoon",
    branch = "harpoon2",
@@ -5,7 +7,49 @@ return {
 
    event = "VimEnter",
    config = function()
-      local keymaps_module = require("keymaps")
-      keymaps_module.init_harpoon_keymaps()
+      local harpoon = require("harpoon")
+
+      vim.keymap.set("n", "<leader>aa", function()
+         harpoon:list():add()
+      end, {
+         desc = "Add to harpoon2",
+      })
+
+      vim.keymap.set("n", "<leader>ad", function()
+         harpoon:list():remove()
+      end, {
+         desc = "Delete from Harpoon2",
+      })
+
+      vim.keymap.set("n", "<leader>e", function()
+         harpoon.ui:toggle_quick_menu(harpoon:list())
+      end, {
+         desc = "Toggle harpoon2 quick menu",
+      })
+
+      vim.keymap.set("n", "<leader>1", function()
+         harpoon:list():select(1)
+      end)
+      vim.keymap.set("n", "<leader>2", function()
+         harpoon:list():select(2)
+      end)
+      vim.keymap.set("n", "<leader>3", function()
+         harpoon:list():select(3)
+      end)
+      vim.keymap.set("n", "<leader>4", function()
+         harpoon:list():select(4)
+      end)
+
+      -- Toggle previous & next buffers stored within Harpoon list
+      vim.keymap.set("n", "<A-TAB>", function()
+         harpoon:list():next()
+      end, {
+         desc = "Go next buffer via harpoon2",
+      })
+      vim.keymap.set("n", "<A-S-TAB>", function()
+         harpoon:list():prev()
+      end, {
+         desc = "Go prev buffer via harpoon2",
+      })
    end,
 }
