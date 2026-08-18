89120e09b55cbc3391f0eb488ff53cb673782315
sNj <Sh1nJiTEITA.TWSOL@gmail.com>
2026-08-19 00:21:26 +0300

    Add auto diff buf open in neotree

--- lua/cneotree.lua
+++ lua/cneotree.lua
@@ -1,3 +1,22 @@
+-- return {
+-- 	gh = "nvim-neo-tree/neo-tree.nvim",
+-- 	deps = {
+-- 		{ gh = "nvim-lua/plenary.nvim" },
+-- 		{ gh = "MunifTanjim/nui.nvim" },
+-- 		{ gh = "nvim-tree/nvim-web-devicons" },
+-- 	},
+--
+-- 	config = function()
+-- 		local mod = require("neo-tree")
+--
+-- 		vim.keymap.set("n", "<C-t>", function()
+-- 			vim.cmd("Neotree toggle")
+-- 		end, { desc = "[T]oggle neotree" })
+--
+-- 		mod.setup({})
+-- 	end,
+-- }
+
 return {
 	gh = "nvim-neo-tree/neo-tree.nvim",
 	deps = {
@@ -5,7 +24,6 @@ return {
 		{ gh = "MunifTanjim/nui.nvim" },
 		{ gh = "nvim-tree/nvim-web-devicons" },
 	},
-
 	config = function()
 		local mod = require("neo-tree")
 
@@ -13,6 +31,40 @@ return {
 			vim.cmd("Neotree toggle")
 		end, { desc = "[T]oggle neotree" })
 
-		mod.setup({})
+		-- open the file, then start gitsigns diff once it has attached
+		local function open_and_diff(state)
+			local node = state.tree:get_node()
+			require("neo-tree.sources.common.commands").open(state)
+			if node.type ~= "file" then
+				return
+			end
+
+			local buf = vim.api.nvim_get_current_buf()
+			local tries = 0
+			local function try()
+				if vim.b[buf].gitsigns_status_dict then
+					require("gitsigns").diffthis()
+				elseif tries < 20 then
+					tries = tries + 1
+					vim.defer_fn(try, 25)
+				end
+			end
+			vim.schedule(try)
+		end
+
+		mod.setup({
+			git_status = {
+				window = {
+					mappings = {
+						["<cr>"] = "open_and_diff",
+						["o"] = "open_and_diff",
+						["D"] = "open", -- plain open, no diff
+					},
+				},
+				commands = {
+					open_and_diff = open_and_diff,
+				},
+			},
+		})
 	end,
 }
